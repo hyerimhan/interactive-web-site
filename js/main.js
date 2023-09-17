406fec46ac5f4195ebd0ccb9ac2b2b346bbe435c
team7 <hnhyerim@gmail.com>
2023-09-18 01:00:01 +0900

    Feat: 스크롤 scene 섹션 관찰자

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 // 디바이스 별로 창 사이즈 변경에 대응하기 위해 따로 함수로 처리한다.
 ;(() => {
+  let yOffset = 0 // window.pageOffset 대신 쓸 변수
+  let prevScrollHeight = 0 // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+  let currentScene = 0 // 현재 활성화된(눈 앞에 보고있는) 씬(scroll-section)
+
   // 환경 변수
   const sceneInfo = [
     {
@@ -49,7 +53,30 @@
 
     console.log(sceneInfo)
   }
-  // 디바이스의 화면 높이에 맞춰 resize됨
-  window.addEventListener('resize', setLayout)
+
+  // 몇 번째 스크롤 섹션인지 판별하기 위한 함수
+  function scrollLoop() {
+    prevScrollHeight = 0
+    for (let i = 0; i < currentScene; i++) {
+      prevScrollHeight += sceneInfo[i].scrollHeight
+    }
+
+    if (yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight) {
+      currentScene++
+    }
+    if (yOffset < prevScrollHeight) {
+      if (currentScene === 0) return // 브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지하기 위한 안전장치(모바일)
+      currentScene--
+    }
+
+    console.log(currentScene)
+  }
+
+  window.addEventListener('resize', setLayout) // 디바이스의 화면 높이에 맞춰 resize됨
+  window.addEventListener('scroll', () => {
+    yOffset = window.pageYOffset
+    scrollLoop()
+  })
+
   setLayout()
 })()
