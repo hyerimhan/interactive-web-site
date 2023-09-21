57d6856b614e7f9c92e04dfeb0bfbb160d509142
hyerimhan <hnhyerim@gmail.com>
2023-09-21 22:28:15 +0900

    Feat: canvas 창 사이즈 맞추기

--- js/main.js
+++ js/main.js
@@ -136,6 +136,11 @@
       }
     }
     document.body.setAttribute('id', `show-scene-${currentScene}`)
+
+    // canvas의 초기 인라인 스타일의 height를 1080으로 정해놓았기 때문에 heightRatio로 각 디바이스의 화면 크기에 맞도록 설정해춘다.
+    const heightRatio = window.innerHeight / 1080
+    console.log(heightRatio)
+    sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio}) `
   }
 
   function calcValues(values, currentYOffset) {
