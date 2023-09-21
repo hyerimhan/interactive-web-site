bbfda2f89c1caf2eb192a10891556302b91ada9c
hyerimhan <hnhyerim@gmail.com>
2023-09-22 01:44:22 +0900

    Feat: 두번째 스크롤 비디오 처리

--- js/main.js
+++ js/main.js
@@ -25,6 +25,7 @@
       values: {
         videoImageCount: 300,
         imageSequence: [0, 299],
+        canvas_opacity: [1, 0, { start: 0.9, end: 1 }],
         // 변화하는 opacity 값의 시작값과 끝값, {애니메이션이 재생되는 구간 설정}
         messageA_opacity_in: [0, 1, { start: 0.1, end: 0.2 }],
         messageB_opacity_in: [0, 1, { start: 0.3, end: 0.4 }],
@@ -66,26 +67,29 @@
         messageC: document.querySelector('#scroll-section-2 .c'),
         pinB: document.querySelector('#scroll-section-2 .b .pin'),
         pinC: document.querySelector('#scroll-section-2 .c .pin'),
+        canvas: document.querySelector('#video-canvas-1'),
+        context: document.querySelector('#video-canvas-1').getContext('2d'),
+        videoImages: [],
       },
       values: {
+        videoImageCount: 960,
+        imageSequence: [0, 959],
+        canvas_opacity_in: [0, 1, { start: 0, end: 0.1 }],
+        canvas_opacity_out: [1, 0, { start: 0.95, end: 1 }],
         messageA_translateY_in: [20, 0, { start: 0.15, end: 0.2 }],
-        messageB_translateY_in: [30, 0, { start: 0.5, end: 0.55 }],
-        messageC_translateY_in: [30, 0, { start: 0.72, end: 0.77 }],
-        messageA_opacity_in: [0, 1, { start: 0.15, end: 0.2 }],
-        messageB_opacity_in: [0, 1, { start: 0.5, end: 0.55 }],
-        messageC_opacity_in: [0, 1, { start: 0.72, end: 0.77 }],
-        messageA_translateY_out: [0, -20, { start: 0.3, end: 0.35 }],
-        messageB_translateY_out: [0, -20, { start: 0.58, end: 0.63 }],
-        messageC_translateY_out: [0, -20, { start: 0.85, end: 0.9 }],
-        messageA_opacity_out: [1, 0, { start: 0.3, end: 0.35 }],
-        messageB_opacity_out: [1, 0, { start: 0.58, end: 0.63 }],
-        messageC_opacity_out: [1, 0, { start: 0.85, end: 0.9 }],
-        pinB_scaleY: [0.5, 1, { start: 0.5, end: 0.55 }],
-        pinC_scaleY: [0.5, 1, { start: 0.72, end: 0.77 }],
-        pinB_opacity_in: [0, 1, { start: 0.5, end: 0.55 }],
-        pinC_opacity_in: [0, 1, { start: 0.72, end: 0.77 }],
-        pinB_opacity_out: [1, 0, { start: 0.58, end: 0.63 }],
-        pinC_opacity_out: [1, 0, { start: 0.85, end: 0.9 }],
+        messageB_translateY_in: [30, 0, { start: 0.6, end: 0.65 }],
+        messageC_translateY_in: [30, 0, { start: 0.87, end: 0.92 }],
+        messageA_opacity_in: [0, 1, { start: 0.25, end: 0.3 }],
+        messageB_opacity_in: [0, 1, { start: 0.6, end: 0.65 }],
+        messageC_opacity_in: [0, 1, { start: 0.87, end: 0.92 }],
+        messageA_translateY_out: [0, -20, { start: 0.4, end: 0.45 }],
+        messageB_translateY_out: [0, -20, { start: 0.68, end: 0.73 }],
+        messageC_translateY_out: [0, -20, { start: 0.95, end: 1 }],
+        messageA_opacity_out: [1, 0, { start: 0.4, end: 0.45 }],
+        messageB_opacity_out: [1, 0, { start: 0.68, end: 0.73 }],
+        messageC_opacity_out: [1, 0, { start: 0.95, end: 1 }],
+        pinB_scaleY: [0.5, 1, { start: 0.6, end: 0.65 }],
+        pinC_scaleY: [0.5, 1, { start: 0.87, end: 0.92 }],
       },
     },
     {
@@ -102,12 +106,21 @@
   ]
 
   function setCanvasImages() {
+    // 0
     let imgElem
     for (let i = 0; i < sceneInfo[0].values.videoImageCount; i++) {
       // new Image() = document.createElement('img')
       imgElem = new Image()
       imgElem.src = `./video/001/IMG_${6726 + i}.JPG`
       sceneInfo[0].objs.videoImages.push(imgElem)
+    }
+
+    // 2
+    let imgElem2
+    for (let i = 0; i < sceneInfo[2].values.videoImageCount; i++) {
+      imgElem2 = new Image()
+      imgElem2.src = `./video/002/IMG_${7027 + i}.JPG`
+      sceneInfo[2].objs.videoImages.push(imgElem2)
     }
   }
   setCanvasImages()
@@ -117,7 +130,6 @@
     for (let i = 0; i < sceneInfo.length; i++) {
       if (sceneInfo[i].type === 'sticky') {
         sceneInfo[i].scrollHeight = sceneInfo[i].heightNum * window.innerHeight
-        sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight}px`
       } else if (sceneInfo[i].type === 'normal') {
         // 설정된 높이값 없애기 (그냥 컨텐츠 높이)
         sceneInfo[i].scrollHeight = sceneInfo[i].objs.container.offsetHeight
@@ -139,8 +151,8 @@
 
     // canvas의 초기 인라인 스타일의 height를 1080으로 정해놓았기 때문에 heightRatio로 각 디바이스의 화면 크기에 맞도록 설정해춘다.
     const heightRatio = window.innerHeight / 1080
-    console.log(heightRatio)
-    sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio}) `
+    sceneInfo[0].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`
+    sceneInfo[2].objs.canvas.style.transform = `translate3d(-50%, -50%, 0) scale(${heightRatio})`
   }
 
   function calcValues(values, currentYOffset) {
@@ -179,6 +191,7 @@
       case 0:
         let sequence = Math.round(calcValues(values.imageSequence, currentYOffset))
         objs.context.drawImage(objs.videoImages[sequence], 0, 0)
+        objs.canvas.style.opacity = calcValues(values.canvas_opacity, currentYOffset)
 
         // 스크롤 위치에 따른 텍스트 위치 변화
         if (scrollRatio <= 0.22) {
@@ -224,7 +237,20 @@
         }
         break
       case 2:
-        if (scrollRatio <= 0.25) {
+        let sequence2 = Math.round(calcValues(values.imageSequence, currentYOffset))
+        objs.context.drawImage(objs.videoImages[sequence2], 0, 0)
+
+        // canvas 애니메이션
+        if (scrollRatio <= 0.5) {
+          // in
+          objs.canvas.style.opacity = calcValues(values.canvas_opacity_in, currentYOffset)
+        } else {
+          // out
+          objs.canvas.style.opacity = calcValues(values.canvas_opacity_out, currentYOffset)
+        }
+
+        // 텍스트 애니메이션
+        if (scrollRatio <= 0.32) {
           // in
           objs.messageA.style.opacity = calcValues(values.messageA_opacity_in, currentYOffset)
           objs.messageA.style.transform = `translate3d(0, ${calcValues(values.messageA_translateY_in, currentYOffset)}%, 0)`
@@ -234,7 +260,7 @@
           objs.messageA.style.transform = `translate3d(0, ${calcValues(values.messageA_translateY_out, currentYOffset)}%, 0)`
         }
 
-        if (scrollRatio <= 0.57) {
+        if (scrollRatio <= 0.67) {
           // in
           objs.messageB.style.transform = `translate3d(0, ${calcValues(values.messageB_translateY_in, currentYOffset)}%, 0)`
           objs.messageB.style.opacity = calcValues(values.messageB_opacity_in, currentYOffset)
@@ -246,7 +272,7 @@
           objs.pinB.style.transform = `scaleY(${calcValues(values.pinB_scaleY, currentYOffset)})`
         }
 
-        if (scrollRatio <= 0.83) {
+        if (scrollRatio <= 0.93) {
           // in
           objs.messageC.style.transform = `translate3d(0, ${calcValues(values.messageC_translateY_in, currentYOffset)}%, 0)`
           objs.messageC.style.opacity = calcValues(values.messageC_opacity_in, currentYOffset)
@@ -292,6 +318,9 @@
     scrollLoop()
   })
   // 'DOMContentLoaded': DOM이 로드됐을때 실행 | 'load': DOM + 이미지가 로드됐을때 실행
-  window.addEventListener('load', setLayout)
+  window.addEventListener('load', () => {
+    setLayout()
+    sceneInfo[0].objs.context.drawImage(sceneInfo[0].objs.videoImages[0], 0, 0)
+  })
   window.addEventListener('resize', setLayout) // 디바이스의 화면 높이에 맞춰 resize됨
 })()
